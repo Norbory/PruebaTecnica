3b6dc0afd99f3b80f6e86a4f7f0bd9773622eac3
Norbory <mandros.angelof@pucp.edu.pe>
2024-04-29 12:53:55 -0500

    First Mock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { FooterComponent } from './components/footer/footer.component';
+import { Entity } from './core/models/entity.interface';
+import { NgFor, NgIf } from '@angular/common';
+import * as data from './data/entities.json';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FooterComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor() { }
   title = 'Proveedores';
+  listEntity: Entity[] = [];
+  selectedEmpresa = {} as Entity;
+  selectedIndex: number = 0;
+  visible: boolean = false;
+
+  empresas = (data as any).default;
+
+  openModal(empresa: Entity, index: number) {
+    this.visible  = true;
+    this.selectedEmpresa = empresa;
+    this.selectedIndex = index;
+  }
+  cerrarModal() {
+    this.visible = false;
+  }
 }
---
+++ src/app/core/models/entity.interface.ts
@@ -0,0 +1,12 @@
+export interface Entity {
+    Razon: string;
+    NombreEmpresa: string;
+    IdentificacionFiscal : number;
+    NumeroTelefono: number;
+    CorreoElectronico: string;
+    SitioWeb: string;
+    Direccion: string;
+    Pais: string;
+    Facturacion: number;
+    FechaEdicion: Date;
+}
